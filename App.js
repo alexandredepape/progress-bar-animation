35b962f5b4de8e731c16ba915c4820d3ac5476ba
Alexandre de Pape <alexandredepape@gmail.com>
2022-09-19 17:33:23 +0200

    wip

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, StyleSheet } from "react-native";
+import { ProgressBar } from "react-native-paper";
+import { useEffect, useRef } from "react";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const progressBarValue = useRef(new Animated.Value(0));
+
+    useEffect(() => {
+      Animated.timing(progressBarValue.current, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true,
+        }).start();
+    }, []);
+
+    return (
+        <Animated.View style={styles.progressBarContainer}>
+            <ProgressBar style={styles.progressBar}
+                         progress={progressBarValue.current}
+                         color={"#9dd4ae"}/>
+        </Animated.View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    progressBarContainer: {
+        flex: 1,
+        backgroundColor: "#313845",
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    progressBar: {
+        width: 300,
+        // backgroundColor: "white",
+        borderRadius: 7.5,
+        height: 15,
+        margin: 10,
+
+    }
 });
